f3ecc01dc6d654796ff31441ba2646c086abe058
Amanda Dourado <amanda_lopes1998@hotmail.com>
2023-05-13 11:36:40 -0300

    Move game on list

--- src/main/java/com/devsuperior/dslist/services/GameListService.java
+++ src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -8,7 +8,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dslist.dto.GameListDTO;
 import com.devsuperior.dslist.entities.GameList;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameListRepository;
+import com.devsuperior.dslist.repositories.GameRepository;
 
 @Service
 public class GameListService {
@@ -16,10 +18,31 @@ public class GameListService {
 	@Autowired
 	private GameListRepository gameListRepository;
 	
+	@Autowired
+	private GameRepository gameRepository;
+	
 	
 	@Transactional(readOnly = true)
 	public List<GameListDTO> findAll(){
 		List<GameList> result = gameListRepository.findAll();
 		return result.stream().map(x -> new GameListDTO(x)).toList();
+	}
+	
+	@Transactional
+	public void move(Long listId, int sourceIndex, int destionationIndex) {
+		List<GameMinProjection> list = gameRepository.searchByList(listId);
+		
+		GameMinProjection obj = list.remove(sourceIndex);
+		list.add(destionationIndex, obj);
+		//a lista de games já está na ordem correta, só precisar alterar o número da posição 
+		
+		// achar o minimo e o maximo da lista, para não precisar atualizar a lista inteira no banco de dados, alterar apenas o que estava dentro do intervalo
+		int min = sourceIndex < destionationIndex ? sourceIndex : destionationIndex;
+		int max = sourceIndex < destionationIndex ?  destionationIndex : sourceIndex;
+		
+		for(int i = min; i <= max; i++) {
+			      // o número da lista // pegar o id do jogo // colocar a nova posição
+			gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+		}
 	}
 }
